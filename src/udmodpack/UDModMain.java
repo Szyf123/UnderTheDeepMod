cbe8e9e888b6cef6126b9820baa29001e6c3029c
Szyf123 <3411327940@qq.com>
2026-09-16 20:36:09 +0800

    基础设置

---
+++ src/udmodpack/UDModMain.java
@@ -0,0 +1,17 @@
+package udmodpack;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class UDModMain extends Mod{
+
+    public UDModMain(){
+        Log.info("Loaded UDModMain constructor.");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
