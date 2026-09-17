fb27af4673c6a9127c393c9849919fa663f52ed1
Szyf123 <3411327940@qq.com>
2026-09-17 11:40:36 +0800

    基础地板类定义

--- src/udmodpack/UDModMain.java
+++ src/udmodpack/UDModMain.java
@@ -1,17 +1,14 @@
 package udmodpack;
 
-import arc.util.*;
 import mindustry.mod.*;
 
 public class UDModMain extends Mod{
 
-    public UDModMain(){
-        Log.info("Loaded UDModMain constructor.");
-    }
-
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        new UdBasicFloor("ud-test-basic-floor", 4){{
+            inEditor = true;
+            alwaysUnlocked = true;
+        }};
     }
-
 }
---
+++ src/udmodpack/UdBasicFloor.java
@@ -0,0 +1,13 @@
+package udmodpack;
+
+import mindustry.world.blocks.environment.Floor;
+
+public class UdBasicFloor extends Floor {
+    public UdBasicFloor(String name) {
+        super(name);
+    }
+
+    public UdBasicFloor(String name, int variants) {
+        super(name, variants);
+    }
+}
